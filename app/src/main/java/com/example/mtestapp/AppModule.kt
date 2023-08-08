ff6748d5ba85430df4553d8d9c7b3307a4ebc075
brian <brianombiro@gmail.com>
2023-08-07 21:59:53 -0500

    ass 3

---
+++ app/src/main/java/com/example/mtestapp/AppModule.kt
@@ -0,0 +1,35 @@
+package com.example.mtestapp
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Provides
+    fun provideRetrofit(moshi: Moshi): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+
+    @Provides
+    fun provideApiService(retrofit: Retrofit): WeatherApi {
+        return retrofit.create(WeatherApi::class.java)
+    }
+}
---
+++ app/src/main/java/com/example/mtestapp/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.mtestapp
+
+object Constants {
+    const val API_KEY = "b8509982dde7235c51502c57be0dbeae"
+}
